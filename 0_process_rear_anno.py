78829d4813208fe89b732da6387a8465bec36b53
zhuMingXu <zhumingxu@pku.edu.cn>
2019-06-26 19:49:34 +0800

    deal with 1000 data

--- 0_process_rear_anno.py
+++ 0_process_rear_anno.py
@@ -1,10 +1,24 @@
 import cv2
 import json
 import os
+import collections
 
-res = json.load(open("annotations.json"))
-images_path = "./images"
+# res = json.load(open("annotations.json"))
+images_path = "./QYS_rear_left_20190323_sihuan_1000"
 
+anno_dir = os.listdir('./annotations')
+
+merge_anno = []
+for i in range(len(anno_dir)):
+    merge_anno.extend(json.load(open(os.path.join('./annotations', anno_dir[i]))))
+
+res = merge_anno
+
+
+# print(res[359])
+
+
+# print(len(merge_anno)),997sample fr:invis r:vis
 
 def oneIsBelongSameSample(vehicle, headlight):
     vehx1 = vehicle['x']
@@ -15,7 +29,9 @@ def oneIsBelongSameSample(vehicle, headlight):
     hly1 = headlight['y']
     hlx2 = headlight['x'] + headlight['width']
     hly2 = headlight['y'] + headlight['height']
-    if vehx1 <= hlx1 <= vehx2 and vehx1 <= hlx2 <= vehx2 and vehy1 <= hly1 <= vehy2 and vehy1 <= hly2 <= vehy2:
+    if hlx2 > 1920:  # fr zhedang
+        hlx2 = 1920
+    if vehx1 - 50 <= hlx1 <= vehx2 and vehx1 <= hlx2 <= vehx2 and vehy1 <= hly1 <= vehy2 and vehy1 <= hly2 <= vehy2:
         return True
     else:
         return False
@@ -29,8 +45,8 @@ def twoIsBelongSameSample(vehicle_list, matchidx, classInfo):
     elif vehi_2['headlight(h)'] is not None and vehi_1['headlight(h)'] is None:
         return matchidx[0]
     elif vehi_1['headlight(h)'] is None and vehi_2['headlight(h)'] is None:
-        d1 = classInfo['x'] - vehi_1[x] + vehi_1['y'] + vehi_1['height'] - classInfo['y']
-        d2 = classInfo['x'] - vehi_2[x] + vehi_2['y'] + vehi_2['height'] - classInfo['y']
+        d1 = classInfo['x'] - vehi_1['x'] + vehi_1['y'] + vehi_1['height'] - classInfo['y']
+        d2 = classInfo['x'] - vehi_2['x'] + vehi_2['y'] + vehi_2['height'] - classInfo['y']
         if d1 <= d2:
             return matchidx[0]
         else:
@@ -80,55 +96,99 @@ def process_annp():
                     classInfo['headlight(h)'] = None
                     classInfo['id'] = id
                     id += 1
-            elif classType == 'headlight(h)':
+            elif classType == 'headlight(h)' and classInfo['hl'] == 'headlight_r':
                 # belong to ? vehicle
                 matchVehIdx = []
                 for vhInfoIdx, vhInfo in enumerate(newClassList):
                     if oneIsBelongSameSample(vhInfo, classInfo):
                         matchVehIdx.append(vhInfoIdx)
-                    print(vhInfoIdx)
+                print("r:", matchVehIdx)
+                assert len(matchVehIdx) == 1 or len(matchVehIdx) == 2 or len(matchVehIdx) == 0 or len(matchVehIdx) == 3
                 if len(matchVehIdx) == 1:  # one match
                     if newClassList[matchVehIdx[0]]['headlight(h)'] is None:
                         newClassList[matchVehIdx[0]]['headlightVisible'] = 1
                         newClassList[matchVehIdx[0]]['headlight(h)'] = classInfo
                     else:
                         continue
-                else:
+                elif len(matchVehIdx) == 0:
+                    continue
+                elif len(matchVehIdx) == 2:
                     # go on match
                     idx = twoIsBelongSameSample(newClassList, matchVehIdx, classInfo)
                     if idx != -1:
                         newClassList[idx]['headlight(h)'] = classInfo
                     else:
                         continue
-                print(matchVehIdx)
-            elif classType == "invisheadlight(h)":
+                elif len(matchVehIdx) == 3:
+                    idx1 = twoIsBelongSameSample(newClassList, [matchVehIdx[0], matchVehIdx[1]], classInfo)
+                    idx2 = twoIsBelongSameSample(newClassList, [matchVehIdx[1], matchVehIdx[2]], classInfo)
+                    idx3 = twoIsBelongSameSample(newClassList, [matchVehIdx[0], matchVehIdx[2]], classInfo)
+                    num = [idx1, idx2, idx3]
+                    matchNum = list(collections.Counter(num).keys())[0]
+                    newClassList[matchNum]['headlight(h)'] = classInfo
+
+            elif classType == "headlight(h)" and classInfo['hl'] == 'headlight_fr':
                 # belong to ? vehicle
-                pass
+                matchVehIdx = []
+                for vhInfoIdx, vhInfo in enumerate(newClassList):
+                    if oneIsBelongSameSample(vhInfo, classInfo):
+                        matchVehIdx.append(vhInfoIdx)
+                print("fr:", matchVehIdx)
+                assert len(matchVehIdx) == 1 or len(matchVehIdx) == 2 or len(matchVehIdx) == 0 or len(matchVehIdx) == 3
+                if len(matchVehIdx) == 1:  # one match
+                    if newClassList[matchVehIdx[0]]['headlight(h)'] is None:
+                        newClassList[matchVehIdx[0]]['headlightVisible'] = 0
+                        newClassList[matchVehIdx[0]]['headlight(h)'] = classInfo
+                    else:
+                        continue
+                elif len(matchVehIdx) == 2:
+                    # go on match
+                    idx = twoIsBelongSameSample(newClassList, matchVehIdx, classInfo)
+                    if idx != -1:
+                        newClassList[idx]['headlight(h)'] = classInfo
+                    else:
+                        continue
+                elif len(matchVehIdx) == 3:
+                    idx1 = twoIsBelongSameSample(newClassList, [matchVehIdx[0], matchVehIdx[1]], classInfo)
+                    idx2 = twoIsBelongSameSample(newClassList, [matchVehIdx[1], matchVehIdx[2]], classInfo)
+                    idx3 = twoIsBelongSameSample(newClassList, [matchVehIdx[0], matchVehIdx[2]], classInfo)
+                    num = [idx1, idx2, idx3]
+                    matchNum = list(collections.Counter(num).keys())[0]
+                    newClassList[matchNum]['headlight(h)'] = classInfo
+                elif len(matchVehIdx) == 0:
+                    continue
             else:
                 continue
             if classType == 'vehicle(v)':
                 newClassList.append(classInfo)
-            # cv2.rectangle(img, (int(x), int(y)), (int(x + w), int(y + h)), (0, 0, 255), 3)
-            # center_x = int(x)
-            # center_y = int(y + 5)
-            # cv2.putText(img, str(j), (center_x, center_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
         newImgAnno['annotations'] = newClassList
         newImgAnno['class'] = imgAnno['class']
         newImgAnno['filename'] = imgAnno['filename']
         totalAnnoList.append(newImgAnno)
+        print("done:", i)
     return totalAnnoList
 
 
-# totalAnnoList = process_annp()
-# with open("new_annotations.json", "w") as f:
-#    f.write(json.dumps(totalAnnoList, indent=4))
+totalAnnoList = process_annp()
+import random
+
+random.shuffle(totalAnnoList)
+train_list = totalAnnoList[0:800]
+test_list = totalAnnoList[800::]
+print("train:", len(train_list))
+print("test:", len(test_list))
+with open("train.json", "w") as f:
+    f.write(json.dumps(train_list, indent=4))
+with open("test.json", "w") as f:
+    f.write(json.dumps(test_list, indent=4))
+
 
 # vis anno
 def vis_new_anno(path="new_annotations.json"):
     res = json.load(open(path))
     for i in range(0, len(res)):
         imgAnno = res[i]
-        imgName = imgAnno['filename'].split('//')[-1]
+        imgName = imgAnno['filename'].split('\\')[-1]
         img = cv2.imread(os.path.join(images_path, imgName))
         classAnno = imgAnno['annotations']
         for j in range(len(classAnno)):
@@ -159,6 +219,8 @@ def vis_new_anno(path="new_annotations.json"):
                 plot_hy = int(hy + 5)
                 cv2.putText(img, str(j), (plot_hx, plot_hy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
         cv2.imshow("img:" + str(i), img)
+        if i == 20:
+            break
     cv2.waitKey()
 
 
